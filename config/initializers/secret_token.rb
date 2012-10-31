da24bc9b00bddf177d24349309f2d7578cdf3789
rob <rob@Quasar.(none)>
2012-10-31 17:04:29 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waf::Application.config.secret_token = 'e8d5ede7f8f5032c79ca20ea6fd6190a2ee26eaac95368c4440c95fdf1f1f47113e97c995fd38de76019bd9fdaf5bee36d1e79c05cf5ce345845942ab91a42a2'
